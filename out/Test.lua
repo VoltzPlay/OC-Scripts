8c3d65d4f2665c1f1faf25b1f9d870ab103b2547
halvors <halvors@halvors.org>
2019-04-06 23:25:36 +0200

    Hahaahaha, zenoy is failing...

--- out/Test.lua
+++
@@ -1,8 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-component = require("component");
-require("TestPage");
-main = function(self)
-    local page = testpage.new(Gpu.new(nil));
-    page:draw();
-end;
-main(_G);
