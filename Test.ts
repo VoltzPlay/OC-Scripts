11685006cb534e4fb02e4f9aae282113197a3242
zenoy <zenoy@zenoy.org>
2019-04-06 23:04:12 +0200

    fix typo testPage

--- Test.ts
+++ Test.ts
@@ -2,7 +2,7 @@
 declare function print(this: void,text:string):void;
 declare function require(this: void,text:string);
 var component = require("component");
-require("Testpage")
+require("TestPage")
 function main()
 {
     let page:Page = new testpage(new Gpu(null))
